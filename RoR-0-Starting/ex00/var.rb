9eccc7b8558375ad447410fc2adf42f17a27db53
jack <jack.sng@gmail.com>
2025-11-22 23:53:07 +0800

    ex00 of ror-0-starting completed

---
+++ RoR-0-Starting/ex00/var.rb
@@ -0,0 +1,14 @@
+def my_var
+    a = 10
+    b = "10"
+    c = nil
+    d = 10.0
+
+    puts "my variables :"
+    puts "a contains: #{a} and is a type: #{a.class}"
+    puts "b contains: #{b} and is a type: #{b.class}"
+    puts "c contains: #{c.inspect} and is a type: #{c.class}"
+    puts "d contains: #{d} and is a type: #{d.class}"
+end
+
+my_var
