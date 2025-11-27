567d9fda7fa958fbd5b3b3795170fcdb1a54a24c
jack <jack.sng@gmail.com>
2025-11-27 10:47:56 +0800

    added is_state boolean check

--- RoR-0-Starting/ex05/whereto.rb
+++ RoR-0-Starting/ex05/whereto.rb
@@ -1,30 +1,47 @@
 #!/usr/bin/env -S ruby -w
-DEBUG = false
+DEBUG = true
+
+def is_state?(states,name)
+    if states.key?(name)
+        return true
+    else
+        return false
+    end
+end
+
+
+def is_capital?
 
 def whereto
-    # states = {
-    #     "Oregon" => "OR",
-    #     "Alabama" => "AL",
-    #     "New Jersey" => "NJ",
-    #     "Colorado" => "CO"
-    # }
-    # capitals_cities = {
-    #     "OR" => "Salem",
-    #     "AL" => "Montgomery",
-    #     "NJ" => "Trenton",
-    #     "CO" => "Denver"
-    # }
+    states = {
+        "Oregon" => "OR",
+        "Alabama" => "AL",
+        "New Jersey" => "NJ",
+        "Colorado" => "CO"
+    }
+    capitals_cities = {
+        "OR" => "Salem",
+        "AL" => "Montgomery",
+        "NJ" => "Trenton",
+        "CO" => "Denver"
+    }
 
     if ARGV.size != 1
         return
     end
+
     input = ARGV[0].split(",")
+
     if DEBUG
         input.map { | name | name.strip! }
+        input.map { | name | name.downcase! }
         input.each do | name |
             puts name
         end
     end
+
+
+
 end
 
 whereto
