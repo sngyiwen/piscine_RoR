114b8ad4f01ae69e26866af97311f028c39a7428
jack <jack.sng@gmail.com>
2025-11-28 12:27:34 +0800

    ex05 completed

--- RoR-0-Starting/ex05/whereto.rb
+++ RoR-0-Starting/ex05/whereto.rb
@@ -9,9 +9,16 @@ def is_state?(states,name)
     end
 end
 
+def is_capital?(capitals_cities, name)
+    capital_city_name = name
+    capitals_cities.each do |code, city|
+        if capital_city_name == city
+           return true
+        end
+    end
+    return false
+end 
 
-def is_capital?
-end
 def whereto
     states = {
         "Oregon" => "OR",
@@ -31,7 +38,7 @@ def whereto
     end
 
     input = ARGV[0].split(",")
-
+    
     input.map! { | name | name.strip }
     input.map! { | name | name.capitalize }
     if DEBUG
@@ -40,22 +47,35 @@ def whereto
         end
     end
     input.each do | name |
+        abbrevation = nil
+        capital_city_name = nil
+
         if name == "New jersey"
             abbrevation = "NJ"
             name = "New Jersey"
         end
+        if (!is_state?(states, name) || !is_capital?(capital_cities, name))
+            puts "#{name} is neither a capital city nor a state"
+            return
+        end    
         if is_state?(states, name)
             abbrevation = states[name]
             capital_city_name = capitals_cities[abbrevation]
             puts "#{capital_city_name} is the capital of #{name} (akr: #{abbrevation})"
+        elsif is_capital?(capitals_cities, name)
+            capitals_cities.each do | key, value |
+                if name == value
+                    abbrevation = key
+                    break
+                end
+            end
+            states.each do |key, value|
+                if abbrevation == value
+                    capital_city_name = key
+                end
+            end
+            puts "#{name} is the capital of #{capital_city_name} (akr: #{abbrevation})"
         end
     end
 end
-# $> ./Where.rb Oregon 
-# Salem
-
-# ./erehW.rb Salem 
-# Oregon
-
-
 whereto
