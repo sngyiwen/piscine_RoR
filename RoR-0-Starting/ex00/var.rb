0f8562881f23fe12af49cb259c1cbebb19e103c3
jack <jack.sng@gmail.com>
2025-12-05 16:15:40 +0800

    initial commit for Oob

--- RoR-0-Starting/ex00/var.rb
+++ RoR-0-Starting/ex00/var.rb
@@ -7,10 +7,10 @@ def my_var
     d = 10.0
 
     puts "my variables :"
-    puts "a contains: #{a} and is a type: #{a.class}"
-    puts "b contains: #{b} and is a type: #{b.class}"
+    puts "a contains: #{a.inspect} and is a type: #{a.class}"
+    puts "b contains: #{b.inspect} and is a type: #{b.class}"
     puts "c contains: #{c.inspect} and is a type: #{c.class}"
-    puts "d contains: #{d} and is a type: #{d.class}"
+    puts "d contains: #{d.inspect} and is a type: #{d.class}"
 end
 
 my_var
