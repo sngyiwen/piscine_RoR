b1ac65d4c8650f7e806a8c846a4d171141426448
jack <jack.sng@gmail.com>
2025-12-19 22:29:59 +0800

    correct class done

--- RoR-0-Oob/ex02/ex02.rb
+++ RoR-0-Oob/ex02/ex02.rb
@@ -51,108 +51,51 @@ class Body_closed < StandardError
 
     def show_state
         lines = File.readlines(@filename)
+        lines.each_with_index do |line, idx|
+            if line.strip == "</body>"
+                puts "In #{@filename} body was closed :"
+                puts "> ln :#{idx + 1} </body> : text has been inserted and tag moved at the end of it."
+                return
+            end
+        end
     end
 
     def correct
-        puts "Do not call dump/finish after the body is closed."
+        lines = File.readlines(@filename)
+        body_line_idx = nil
+        html_line_idx = nil
+
+        lines.each_with_index do |line, idx|
+            if line.strip =="</body>"
+                body_line_idx = idx
+            elsif line.strip == "</html>"
+                html_line_idx = idx
+            end
+        end
+
+        if body_line_idx
+
+            new_lines = []
+            lines.each_with_index do |line, idx|
+                unless idx == body_line_idx || idx == html_line_idx
+                    new_lines << line
+                end
+            end
+
+            new_lines << << "    <p>#{@text}</p>\n"
+
+            new_lines << "</body>\n"
+            new_lines << "</html>\n"
+
+            File.write(@filename, new_lines.join)
+        end
     end
 
     def explain
-        puts "Once </body> tag is used to close the body tag, you cannot add more content."
+        puts "The body tag has been reopened, text inserted, and body tag closed again."
     end
 end
 
-class Html
-    attr_reader :page_name
-
-    def initialize(name)
-        @page_name = name
-        @filename = "#{name}.html"
-        @body_open = false
-        @body_closed = false
-        loop do 
-            begin 
-                head
-                break
-            rescue Dup_file => e
-                e.show_state
-                e.correct
-                e.explain
-                @filename = e.new_filename
-                @page_name = File.basename(e.new_filename, ".html")
-            end
-        end
-    end
-
-    def head
-        if File.exist?(@filename) 
-            raise Dup_file.new(@filename)
-        end
-        File.open(@filename, "w") do |f|
-            f.puts"<!DOCTYPE html>"
-            f.puts"<html>"
-            f.puts"<head>"
-            f.puts"<title>#{@page_name}</title>"
-            f.puts"</head>"
-            f.puts"<body>"
-        end
-        @body_open = true
-        nil
-    end
-
-    def dump(str)
-        begin 
-            unless @body_open
-                raise "There is no body tag in #{@filename}"
-            end
-            if @body_closed
-                raise Body_closed.new(@filename)
-            end
-            line = "    <p>#{str}</p>\n"
-            File.open(@filename, "a") do |f|
-                f.write line
-            end
-            nil
-        rescue Body_closed => e
-            e.show_state
-            e.correct
-            e.explain
-            nil
-        rescue RuntimeError => e
-            puts e.message
-            nil
-        end
-        # line.bytesize
-    end
-    
-    def finish
-        begin 
-            if @body_closed
-                raise Body_closed.new(@filename)
-            end
-
-            unless @body_open
-                raise "There is no body tag in #{@filename}"
-            end
-
-            lines = "</body>\n</html>\n"
-            File.open(@filename, "a") do |f|
-                f.write lines
-            end
-            @body_closed = true
-            nil
-        rescue Body_closed => e
-            e.show_state
-            e.correct
-            e.explain
-            nil
-        rescue RuntimeError => e
-            puts e.message
-            nil
-        end
-        # lines.bytesize
-    end
-end
 
 if $PROGRAM_NAME == __FILE__
     a = Html.new("test")
