1d6c09b0ef13404f08fb449e475ec6dba064dd29
jack <jack.sng@gmail.com>
2026-01-14 11:14:28 +0800

    rewrote ex04 to comply to pdf's specificity

--- RoR-0-Oob/ex04/ex04.rb
+++ RoR-0-Oob/ex04/ex04.rb
@@ -1,7 +1,65 @@
 #!/usr/bin/env -S ruby -w
 
-require_relative '../ex03/ex03.rb'
+class Text 
+    attr_reader :content
+    def initialize(content)
+        @content = content
+    end
+    def to_s
+        @content 
+    end
+end
 
+class Elem
+    attr_reader :tag, :content, :tag_type, :opt
+    
+    def initialize(tag, content = [], tag_type = 'double', opt = {})
+        @tag = tag
+        @content = content
+        @opt = opt
+        @tag_type = tag_type
+    end
+
+    def add_content(*new_content)
+        @content = [@content] unless @content.is_a?(Array)
+        @content += new_content.flatten(1)
+    end
+
+    def to_s
+        attrs = format_attributes
+        if @tag_type == 'simple'
+            result = "<#{@tag}#{attrs} />"
+        elsif @tag_type == 'double'
+            result = "<#{@tag}#{attrs}>"
+
+            if @content.is_a?(Text)
+                result += @content.to_s
+            elsif @content.is_a?(String)
+                result += @content.empty? ? "\n" : @content
+            elsif @content.is_a?(Array)
+                result += "\n"
+                @content.each do |item|
+                    if item.is_a?(Elem)
+                        result += item.to_s + "\n"
+                    else
+                        result += item.to_s + "\n"
+                    end
+                end
+            else
+                result += "\n"
+            end
+            result += "</#{@tag}>"
+        end
+    end    
+    private
+
+    def format_attributes
+        if @opt.empty?
+            return ""
+        end
+        " " + @opt.map { |key, value| "#{key}='#{value}'" }.join(" ")
+    end 
+end
 class P < Elem
     def initialize(content = [], opt = {})
         super('P', content, 'double', opt)
